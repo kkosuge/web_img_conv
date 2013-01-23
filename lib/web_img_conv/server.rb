d5647aab2a101babdc806c34e1aafbf68b41a228
kkosuge <kkosuge@about.me>
2013-01-23 16:16:23 +0900

    production環境でエラーが起こったときのメッセージ

--- lib/web_img_conv/server.rb
+++ lib/web_img_conv/server.rb
@@ -10,7 +10,17 @@ module WebImgConv
       data = @image_converter.convert(env['PATH_INFO'])
       if data.is_a? Magick::Image
         [200, {"Content-Type" => data.mime_type}, [data.to_blob]]
+      elsif production?
+        [400, {"Content-Type" => 'text'}, ['Bad Request']]
       end
+    rescue => e
+      if production?
+        [503, {"Content-Type" => 'text'}, [e.to_s]]
+      end
+    end
+
+    def production?
+      ENV['RACK_ENV'] == "production"
     end
   end
 end
